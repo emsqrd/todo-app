66fad58596c83134149d983f31e93250fee2ca8f
Mark <madlyinanger37@gmail.com>
2025-02-08 12:40:25 -0600

    Implement drag-and-drop functionality in TodoList component and enhance styling

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,14 +1,20 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  CdkDragDrop,
+  DragDropModule,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
 
 interface Task {
   id: number;
   title: string;
   dueDate: string;
 }
+
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [DragDropModule],
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
@@ -17,6 +23,8 @@ export class TodoListComponent {
   @ViewChild('taskTitleInput') taskTitleInput!: ElementRef<HTMLInputElement>;
   @ViewChild('taskDateInput') taskDateInput!: ElementRef<HTMLInputElement>;
 
+  bodyElement: HTMLElement = document.body;
+
   private formatDate(date: Date): string {
     return date.toLocaleDateString('en-US', {
       month: 'short',
@@ -24,7 +32,28 @@ export class TodoListComponent {
     });
   }
 
-  tasks: Task[] = [];
+  tasks: Task[] = [
+    {
+      id: Math.random(),
+      title: 'Walk the dog',
+      dueDate: this.formatDate(new Date('02/05/25')),
+    },
+    {
+      id: Math.random(),
+      title: 'Read a book',
+      dueDate: this.formatDate(new Date('02/23/25')),
+    },
+    {
+      id: Math.random(),
+      title: 'Take out the garbage',
+      dueDate: this.formatDate(new Date('02/05/25')),
+    },
+    {
+      id: Math.random(),
+      title: 'Make dinner',
+      dueDate: this.formatDate(new Date('02/07/25')),
+    },
+  ];
 
   onAddTaskClick() {
     this.addTaskDialog.nativeElement.showModal();
@@ -51,5 +80,16 @@ export class TodoListComponent {
 
   onDeleteTaskClick(id: number) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onDragStarted() {
+    this.bodyElement.classList.add('inheritCursors');
+    this.bodyElement.style.cursor = 'grabbing';
+  }
+
+  onDrop(event: CdkDragDrop<Task[]>) {
+    moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+    this.bodyElement.classList.remove('inheritCursors');
+    this.bodyElement.style.cursor = 'unset';
   }
 }
