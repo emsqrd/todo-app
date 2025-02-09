67581afd80eb2364c00cce0bf661d00f38fbfe15
Mark <madlyinanger37@gmail.com>
2025-02-08 19:24:10 -0600

    Refactor task management by creating a standalone Task component and updating the TaskList component to utilize it, enhancing code modularity and readability.

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -5,16 +5,13 @@ import {
   moveItemInArray,
 } from '@angular/cdk/drag-drop';
 
-interface Task {
-  id: number;
-  name: string;
-  dueDate: string;
-}
+import { TaskComponent } from '../task/task.component';
+import { Task } from '../../models/task';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [DragDropModule],
+  imports: [DragDropModule, TaskComponent],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css'],
 })
@@ -36,27 +33,27 @@ export class TaskListComponent {
     {
       id: Math.random(),
       name: 'Walk the dog',
-      dueDate: this.formatDate(new Date('02/05/25')),
+      dueDate: new Date('02/05/25'),
     },
     {
       id: Math.random(),
       name: 'Read a book',
-      dueDate: this.formatDate(new Date('02/23/25')),
+      dueDate: new Date('02/23/25'),
     },
     {
       id: Math.random(),
       name: 'Take out the garbage',
-      dueDate: this.formatDate(new Date('02/05/25')),
+      dueDate: new Date('02/05/25'),
     },
     {
       id: Math.random(),
       name: 'Make dinner',
-      dueDate: this.formatDate(new Date('02/07/25')),
+      dueDate: new Date('02/07/25'),
     },
     {
       id: Math.random(),
       name: 'Do laundry',
-      dueDate: this.formatDate(new Date('02/13/25')),
+      dueDate: new Date('02/13/25'),
     },
   ];
 
@@ -72,7 +69,7 @@ export class TaskListComponent {
     const newTask = {
       id: Math.random(),
       name: name,
-      dueDate: this.formatDate(new Date(date)),
+      dueDate: new Date(date),
     };
 
     this.tasks.push(newTask);
@@ -83,18 +80,13 @@ export class TaskListComponent {
     this.onCancelDialogClick();
   }
 
-  onDeleteTaskClick(id: number) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
-  }
-
-  onDragStarted() {
-    this.bodyElement.classList.add('inheritCursors');
-    this.bodyElement.style.cursor = 'grabbing';
-  }
-
   onDrop(event: CdkDragDrop<Task[]>) {
     moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
     this.bodyElement.classList.remove('inheritCursors');
     this.bodyElement.style.cursor = 'unset';
+  }
+
+  handleDeleteTask(id: number) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../../models/task';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [DragDropModule],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrls: ['./task.component.css'],
 })
 export class TaskComponent {
+  @Input() task!: Task;
+  @Output() deleteTask = new EventEmitter<number>();
 
+  bodyElement: HTMLElement = document.body;
+
+  onDeleteTaskClick(id: number) {
+    this.deleteTask.emit(id);
+  }
+
+  onDragStarted() {
+    this.bodyElement.classList.add('inheritCursors');
+    this.bodyElement.style.cursor = 'grabbing';
+  }
 }
